cc6c0b2e92bd3f769c48f88471a478176005c054
John Munns <john04munns@gmail.com>
2024-09-16 23:24:03 +0000

    Updated static url in settings.py

--- Software_Engineering___Agile_Web_Application/settings.py
+++ Software_Engineering___Agile_Web_Application/settings.py
@@ -109,7 +109,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 # STATIC_ROOT = posixpath.join(*(BASE_DIR.split(os.path.sep) + ['staticfiles']))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
