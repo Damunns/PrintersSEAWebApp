572de89d8f39d16539062fff4eb4f894fd178910
John Munns <john04munns@gmail.com>
2024-09-16 22:55:03 +0000

    Update urls.py

--- Software_Engineering___Agile_Web_Application/urls.py
+++ Software_Engineering___Agile_Web_Application/urls.py
@@ -1,14 +1,14 @@
 """
 Definition of urls for Software_Engineering___Agile_Web_Application.
 """
-
 from datetime import datetime
 from email.mime import application
 from django.urls import path, include
 from django.contrib import admin
 from django.contrib.auth.views import LoginView, LogoutView
 from app import forms, views
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -16,17 +16,19 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
     path('login/',
-         LoginView.as_view
-         (
+         LoginView.as_view(
              template_name='app/login.html',
              authentication_form=forms.BootstrapAuthenticationForm,
-             extra_context=
-             {
+             extra_context={
                  'title': 'Log in',
-                 'year' : datetime.now().year,
+                 'year': datetime.now().year,
              }
          ),
          name='login'),
     path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
     path('admin/', admin.site.urls),
 ]
+
+# Only include this in development
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
