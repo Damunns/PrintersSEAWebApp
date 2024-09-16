7920a998d12c297bc9d672dc619976adcf15f481
John Munns <john04munns@gmail.com>
2024-09-16 22:46:51 +0000

    Change definition of STATIC_ROOT in settings.py

--- Software_Engineering___Agile_Web_Application/settings.py
+++ Software_Engineering___Agile_Web_Application/settings.py
@@ -110,8 +110,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = posixpath.join(*(BASE_DIR.split(os.path.sep) + ['staticfiles']))
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = posixpath.join(*(BASE_DIR.split(os.path.sep) + ['staticfiles']))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, 'app/static'),
