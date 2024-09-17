0cc6989a7d2be3f9401dd233d5c2f2021d7fe6cb
John Munns <john04munns@gmail.com>
2024-09-17 10:59:25 +0000

    Reconfigurations

--- Software_Engineering___Agile_Web_Application/settings.py
+++ Software_Engineering___Agile_Web_Application/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '0d971c4a-cf0c-4ac8-85dd-2edeaf432d31'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', 'localhost']
 
