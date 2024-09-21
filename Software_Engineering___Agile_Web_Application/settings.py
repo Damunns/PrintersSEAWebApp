d3d90f20348cc711f4660212ad26f89270bb7c59
David Munns <munnzed@gmail.com>
2024-09-21 16:00:57 +0000

    Added decouple to requirements.txt

--- Software_Engineering___Agile_Web_Application/settings.py
+++ Software_Engineering___Agile_Web_Application/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 import posixpath
 import dj_database_url
-import decouple
 from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
