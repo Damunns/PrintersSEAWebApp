0de3dfa049370c8ba1fa6b57c7d5c44d1b6555ad
David Munns <munnzed@gmail.com>
2024-09-21 22:21:21 +0000

    Add Printer model import in views.py

--- app/views.py
+++ app/views.py
@@ -5,6 +5,7 @@ Definition of views.
 from datetime import datetime
 from django.shortcuts import render
 from django.http import HttpRequest
+from .models import Printer  # Import the Printer model
 
 def home(request):
     """Renders the home page."""
